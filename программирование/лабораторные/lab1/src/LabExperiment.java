4848417d548047ad6e48f0d69d5007273102713b
maxbarsukov <maximbarsukov@bk.ru>
2022-10-10 09:10:03 +0300

    feat: :sparkles: add additional exercise for prog lab1

---
+++ программирование/лабораторные/lab1/src/LabExperiment.java
@@ -0,0 +1,33 @@
+package src;
+
+import java.util.Iterator;
+
+class InfIterator implements Iterator<Integer> {
+    public Integer value = 0;
+
+    @Override
+    public boolean hasNext() {
+        return true;
+    }
+
+    @Override
+    public Integer next() {
+        return ++value;
+    }
+}
+
+class Inf implements Iterable<Integer> {
+    @Override
+    public Iterator<Integer> iterator() {
+        return new InfIterator();
+    }
+}
+
+public class LabExperiment {
+    public static void main(String[] args) {
+        var inf = new Inf();
+        for(var i : inf){
+            System.out.println(i);
+        }
+    }
+}
