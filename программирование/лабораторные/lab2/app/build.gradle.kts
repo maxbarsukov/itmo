b70f22b7e2fccbcc0b7c66726f5143653de83bc0
maxbarsukov <maximbarsukov@bk.ru>
2022-10-10 15:53:43 +0300

    feat: :sparkles: add gradle task to compile fat jar

--- программирование/лабораторные/lab2/app/build.gradle.kts
+++ программирование/лабораторные/lab2/app/build.gradle.kts
@@ -22,6 +22,24 @@ testing {
     }
 }
 
+tasks {
+    val fatJar = register<Jar>("fatJar") {
+        dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources"))
+        archiveClassifier.set("standalone")
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest { attributes(mapOf("Main-Class" to application.mainClass)) }
+        val sourcesMain = sourceSets.main.get()
+        val contents = configurations.runtimeClasspath.get()
+            .map { if (it.isDirectory) it else zipTree(it) } +
+            sourcesMain.output
+        from(contents)
+    }
+
+    build {
+        dependsOn(fatJar)
+    }
+}
+
 application {
     mainClass.set("org.itmo.prog.lab2.AppKt")
 }
