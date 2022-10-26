7210fccf5215c74ffe698dea216e3eb95fcef5d2
maxbarsukov <maximbarsukov@bk.ru>
2022-10-26 19:10:19 +0300

    test: :rotating_light: add prog#3 test

--- программирование/лабораторные/lab3/app/src/test/java/ru/itmo/prog/lab3/StoryTest.java
+++ программирование/лабораторные/lab3/app/src/test/java/ru/itmo/prog/lab3/StoryTest.java
@@ -1,11 +1,25 @@
 package ru.itmo.prog.lab3;
 
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.*;
 
 public class StoryTest {
-    @Test public void appHasAGreeting() {
-        Main classUnderTest = new Main();
-        assertNotNull("app should have a greeting", 1);
-    }
+  @Test
+  public void appHasAGreeting() {
+    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(outContent));
+
+    String expectedOutput  = "Одним прыжком Знайка достиг беседки и заглянул внутрь.\n" +
+      "Винтика и там не было.\n" +
+      "Коротышки принялись тянуть веревку и притянули Знайку обратно к дому.\n" +
+      "Знайка мгновенно вскарабкался по водосточной трубе на крышу и уже хотел оглядеться по сторонам, но налетевший неожиданно порыв ветра сдул его с крыши и понес в сторону.\n" +
+      "Это не испугало Знайку, так как он знал, что коротышки в любой момент могут притянуть его на веревке обратно.\n";
+
+    Main.main(new String[]{});
+    assertEquals(expectedOutput, outContent.toString());
+  }
 }
