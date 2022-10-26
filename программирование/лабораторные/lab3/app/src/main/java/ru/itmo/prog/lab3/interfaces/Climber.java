40027d28d079801ce1baebd780123f06a742d80a
maxbarsukov <maximbarsukov@bk.ru>
2022-10-26 19:39:32 +0300

    fix: :bug: fix typo

--- программирование/лабораторные/lab3/app/src/main/java/ru/itmo/prog/lab3/interfaces/Climber.java
+++ программирование/лабораторные/lab3/app/src/main/java/ru/itmo/prog/lab3/interfaces/Climber.java
@@ -4,7 +4,7 @@ import ru.itmo.prog.lab3.models.places.Place;
 
 public interface Climber {
   String climb(Climbable climbable);
-  String climb(Climbable climbable, Place palce);
+  String climb(Climbable climbable, Place place);
 
   int calculateTimeToClimb();
 }
