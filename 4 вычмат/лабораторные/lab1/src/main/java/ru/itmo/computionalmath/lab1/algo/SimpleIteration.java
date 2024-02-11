40890eefcec657070c37285e9ade4f8896bd93aa
maxbarsukov <maximbarsukov@bk.ru>
2024-02-11 16:47:07 +0300

    docs(cm): :books: add reports for lab#1

--- 4 вычмат/лабораторные/lab1/src/main/java/ru/itmo/computionalmath/lab1/algo/SimpleIteration.java
+++ 4 вычмат/лабораторные/lab1/src/main/java/ru/itmo/computionalmath/lab1/algo/SimpleIteration.java
@@ -10,34 +10,34 @@ import static java.lang.Math.abs;
 import static ru.itmo.computionalmath.lab1.utils.Color.*;
 
 public class SimpleIteration {
-  private static final Printer pr = new Printer();
-  private static double[][] val;
+  private static final Printer printer = new Printer();
+  private static double[][] data;
 
   public static void compute(Matrix matrix, double eps) {
-    pr.printMatrix(matrix);
+    printer.printMatrix(matrix);
 
     if (checkDiagonal(matrix.getMatrix(), matrix.getSize())) {
       Result rs = methodOfSimpleIterations(matrix, eps);
 
-      pr.println(rs.getTable());
-      pr.printVector(CYAN_BOLD + "Решение системы: " + RESET, rs.getResult());
-      pr.printVector(CYAN_BOLD + "Вектор невязки: " + RESET, rs.getResiduals());
+      printer.println(rs.getTable());
+      printer.printVector(CYAN_BOLD + "Решение системы: " + RESET, rs.getResult());
+      printer.printVector(CYAN_BOLD + "Вектор невязки: " + RESET, rs.getResiduals());
       return;
     }
 
     permuteMatrix(matrix, 0);
-    if (val != null) {
-      Matrix matrix1 = new Matrix(val);
-      pr.println(BLUE + "Матрица после перестановки строк" + RESET);
-      pr.printMatrix(matrix1);
+    if (data != null) {
+      Matrix matrix1 = new Matrix(data);
+      printer.println(BLUE + "Матрица после перестановки строк" + RESET);
+      printer.printMatrix(matrix1);
 
       Result rs = methodOfSimpleIterations(matrix1, eps);
 
-      pr.println(rs.getTable());
-      pr.printVector(CYAN_BOLD + "Решение системы: " + RESET, rs.getResult());
-      pr.printVector(CYAN_BOLD + "Вектор невязки: " + RESET, rs.getResiduals());
+      printer.println(rs.getTable());
+      printer.printVector(CYAN_BOLD + "Решение системы: " + RESET, rs.getResult());
+      printer.printVector(CYAN_BOLD + "Вектор невязки: " + RESET, rs.getResiduals());
     } else {
-      pr.println(RED + "Отсутствие диагонального преобладания" + RESET);
+      printer.println(RED + "Отсутствие диагонального преобладания" + RESET);
     }
   }
 
@@ -62,11 +62,11 @@ public class SimpleIteration {
   private static void permuteMatrix(Matrix matrix, int index) {
     if (index >= matrix.getMatrix().length - 1) {
       if (checkDiagonal(matrix.getMatrix(), matrix.getSize())) {
-        val = new double[matrix.getSize()][matrix.getSize() + 1];
+        data = new double[matrix.getSize()][matrix.getSize() + 1];
 
         for (int i = 0; i < matrix.getSize(); i++) {
           for (int j = 0; j < matrix.getSize() + 1; j++) {
-            val[i][j] = matrix.getMatrix()[i][j];
+            data[i][j] = matrix.getMatrix()[i][j];
           }
         }
       }
@@ -82,7 +82,7 @@ public class SimpleIteration {
         matrix.getMatrix()[index] = matrix.getMatrix()[i];
         matrix.getMatrix()[i] = t;
       }
-      pr.printMatrix(matrix);
+      printer.printMatrix(matrix);
     }
   }
 
@@ -105,8 +105,10 @@ public class SimpleIteration {
         }
         x[i] = (matrix.getVector()[i] - sum) / matrix.getMatrix()[i][i];
         esps.add(abs(x[i] - t));
-        if (abs(x[i] - t) > norma)
+
+        if (abs(x[i] - t) > norma) {
           norma = abs(x[i] - t);
+        }
       }
       rs.addIter(x);
       rs.addE(esps);
@@ -117,6 +119,7 @@ public class SimpleIteration {
     rs.setResult(x);
 
     ArrayList<Double> residuals = new ArrayList<>();
+
     for (int i = 0; i < matrix.getSize(); i++) {
       double S = 0;
       for (int j = 0; j < matrix.getSize(); j++) {
@@ -124,6 +127,7 @@ public class SimpleIteration {
       }
       residuals.add(S - matrix.getVector()[i]);
     }
+
     rs.setResiduals(residuals);
     return rs;
   }
