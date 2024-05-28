89224b9a31b4d71cd841e9c1f88a9a31ef2d0a24
maxbarsukov <maximbarsukov@bk.ru>
2024-05-28 18:12:54 +0300

    feat(cm): :sparkles: update lab#6

--- 4 вычмат/лабораторные/lab6/src/main.py
+++ 4 вычмат/лабораторные/lab6/src/main.py
@@ -1,7 +1,21 @@
-from math import sin, cos, exp
+from math import sin, cos, exp, inf
 import matplotlib.pyplot as plt
+import os, signal
 
 from methods import improved_euler_method, fourth_order_runge_kutta_method, milne_method
+
+
+MAX_ITERS = 20
+
+
+def my_input(str):
+    s = input(str)
+    if (s == 'q'):
+        print("! Выход из программы.")
+        thanks()
+        os.kill(os.getpid(), signal.SIGINT)
+    else:
+        return s
 
 
 def select_odu():
@@ -9,11 +23,12 @@ def select_odu():
     print('1. y + (1 + x)*y^2')
     print('2. x + y')
     print('3. sin(x) - y')
-    print('4. e^x')
+    print('4. y / x')
+    print('5. e^x\n')
 
     while True:
         try:
-            input_func = int(input('> Выберите ОДУ [1/2/3/4]: '))
+            input_func = int(my_input('> Выберите ОДУ [1/2/3/4/5]: '))
             if input_func == 1:
                 f = lambda x, y: y + (1 + x) * y ** 2
                 # Точные значения находим через https://mathdf.com/dif/ru/#expr=y'%3Dy%2B(1%2Bx)*y%5E2&func=y&arg=x&vals=x_0%3By_0
@@ -28,6 +43,10 @@ def select_odu():
                 exact_y = lambda x, x0, y0: (2*exp(x0)* y0-exp(x0)*sin(x0)+exp(x0)*cos(x0)) / (2*exp(x)) + (sin(x)) / 2 - (cos(x)) / 2
                 break
             elif input_func == 4:
+                f = lambda x, y: y / x
+                exact_y = lambda x, x0, y0: (x*y0) / x0
+                break
+            elif input_func == 5:
                 f = lambda x, y: exp(x)
                 exact_y = lambda x, x0, y0: y0 - exp(x0) + exp(x)
                 break
@@ -51,43 +70,77 @@ def draw_plot(a, b, func, x0, y0, dx=0.01):
     plt.plot(xs, ys, 'g')
 
 
-def solve(f, xs, x0, y0, exact_y, eps):
+def solve(f, x0, xn, n, y0, exact_y, eps):
     print()
     methods = [("Усовершенствованный метод Эйлера", improved_euler_method),
                ("Метод Рунге-Кутта 4-го порядка", fourth_order_runge_kutta_method),
                ("Метод Милна", milne_method)]
 
     for name, method in methods:
-        print(name + ":")
+        ni = n
+        print(name + ":\n")
 
-        ys = method(f, xs, y0, eps)
-        print("y:\t[", *map(lambda x: round(x, 5), ys), "]")
-        print("y_точн:\t[", *map(lambda x: round(exact_y(x, x0, y0), 5), xs), "]")
+        try:
+            iters = 0
 
-        if method is milne_method:
-            inaccuracy = max([abs(exact_y(x, x0, y0) - y) for x, y in zip(xs, ys)])
-            print(f"Погрешность (max|y_iточн - y_i|): {inaccuracy}")
-        else:
-            xs2 = []
-            for x1, x2 in zip(xs, xs[1:]):
-                xs2.extend([x1, (x1 + x2) / 2, x2])
-            ys2 = method(f, xs2, y0, eps)
+            xs = [x0 + i * ((xn - x0) / ni) for i in range(ni)]
+            ys = method(f, xs, y0, eps)
+            inaccuracy = inf
 
-            p = 4 if method is fourth_order_runge_kutta_method else 1
-            inaccuracy = max([abs(y1 - y2) / (2 ** p - 1) for y1, y2 in zip(ys, ys2)])
-            print(f"Погрешность (по правилу Рунге): {inaccuracy}")
+            while inaccuracy > eps:
+                if (iters >= MAX_ITERS):
+                    print(f"! Не удалось увеличить точность. Произведено {iters} итераций.")
+                    break
 
-        print('-' * 30 + '\n')
+                iters += 1
+                ni *= 2
+                xs = [x0 + i * (xn - x0) / ni for i in range(ni)]
+                new_ys = method(f, xs, y0, eps)
 
-        plt.title(name)
-        draw_plot(xs[0], xs[-1], exact_y, x0, y0)
+                if method is milne_method:
+                    inaccuracy = max([abs(exact_y(x, x0, y0) - y) for x, y in zip(xs, new_ys)])
+                else:
+                    p = 4 if method is fourth_order_runge_kutta_method else 2
+                    coef = 2**p - 1
+                    inaccuracy = abs(new_ys[-1] - ys[-1]) / coef
 
-        for i in range(len(xs)):
-            plt.scatter(xs[i], ys[i], c='r')
+                ys = new_ys.copy()
 
-        plt.xlabel("X")
-        plt.ylabel("Y")
-        plt.show()
+            #     print("\tDEBUG:", str(iters) + ")", inaccuracy, (">=" if inaccuracy >= eps else "<"), eps)
+            # print()
+
+            if (iters != 1):
+                print(f"Для точности eps={eps} интервал был разбит на n={ni} частей с шагом h={round((xn - x0) / ni, 6)} за {iters} итераций.\n")
+            else:
+                print(f"Для точности eps={eps} интервал был разбит на n={ni} частей с шагом h={round((xn - x0) / ni, 6)}.\n")
+
+            if (len(xs) < 100):
+                print("y:\t[", *map(lambda x: round(x, 5), ys), "]")
+                print("y_точн:\t[", *map(lambda x: round(exact_y(x, x0, y0), 5), xs), "]")
+            elif (my_input("Показать все значения y (количество > 100)? [y/n]: ") == 'y'):
+                print("y:\t[", *map(lambda x: round(x, 5), ys), "]")
+                print("y_точн:\t[", *map(lambda x: round(exact_y(x, x0, y0), 5), xs), "]")
+
+            print()
+            if method is milne_method:
+                print(f"Погрешность (max|y_iточн - y_i|): {inaccuracy}")
+            else:
+                print(f"Погрешность (по правилу Рунге): {inaccuracy}")
+
+            print("\nПостроение графика ...")
+            print('-' * 30 + '\n')
+
+            plt.title(name)
+            draw_plot(xs[0], xs[-1], exact_y, x0, y0)
+            for i in range(len(xs)):
+                plt.scatter(xs[i], ys[i], c='r')
+
+            plt.xlabel("X")
+            plt.ylabel("Y")
+            plt.show()
+        except OverflowError:
+            print('-' * 30 + '\n')
+            print("! Невозможно вычислить. Число/точность слишком большое.")
 
 
 def main():
@@ -95,16 +148,15 @@ def main():
 
     while True:
         try:
-            x0 = float(input('> Введите первый элемент интервала x0: '))
-            h = float(input('> Введите шаг h: '))
-            n = int(input('> Введите количество элементов в интервале n: '))
+            x0 = float(my_input('> Введите первый элемент интервала x0: '))
+            xn = float(my_input('> Введите последний элемент интервала xn: '))
+            n = int(my_input('> Введите количество элементов в интервале n: '))
 
-            xs = [x0 + i * h for i in range(n)]
-            y0 = float(input('> Введите y0: '))
-            eps = float(input('> Введите точность eps: '))
+            y0 = float(my_input('> Введите y0: '))
+            eps = float(my_input('> Введите точность eps: '))
 
-            if h <= 0:
-                print('! Шаг h должен быть положительным. Введите еще раз.')
+            if xn <= x0:
+                print('! xn должен быть больше x0. Введите еще раз.')
             elif n <= 1:
                 print('! Количество элементов n должно быть > 1. Введите еще раз.')
             else:
@@ -112,11 +164,10 @@ def main():
         except:
             print("! Некорректный ввод. Попробуйте еще раз\n")
 
-    solve(f, xs, x0, y0, exact_y, eps)
+    solve(f, x0, xn, n, y0, exact_y, eps)
 
 
-if __name__ == "__main__":
-    main()
+def thanks():
     print("\nСпасибо за использование программы!\n")
     print("""\
                              ,
@@ -132,3 +183,7 @@ if __name__ == "__main__":
        )  |  \  `.___________|/
        `--'   `--'
     """)
+
+if __name__ == "__main__":
+    main()
+    thanks()
--- 4 вычмат/лабораторные/lab6/src/methods.py
+++ 4 вычмат/лабораторные/lab6/src/methods.py
@@ -1,42 +1,49 @@
 def improved_euler_method(f, xs, y0, eps):
     ys = [y0]
     h = xs[1] - xs[0]
-    for i in range(1, len(xs)):
-        y_pred = ys[i-1] + h * f(xs[i-1], ys[i-1])
-        y_corr = ys[i-1] + h * f(xs[i], y_pred)
-        ys.append(0.5 * (y_pred + y_corr))
+    for i in range(len(xs)):
+        y_pred = f(xs[i], ys[i])
+        y_corr = f(xs[i] + h, ys[i] + h * y_pred)
+        ys.append(ys[i] + 0.5 * h * (y_pred + y_corr))
     return ys
 
 
 def fourth_order_runge_kutta_method(f, xs, y0, eps):
     ys = [y0]
     h = xs[1] - xs[0]
-    for i in range(1, len(xs)):
-        k1 = h * f(xs[i - 1], ys[i - 1])
-        k2 = h * f(xs[i - 1] + h / 2, ys[i - 1] + k1 / 2)
-        k3 = h * f(xs[i - 1] + h / 2, ys[i - 1] + k2 / 2)
-        k4 = h * f(xs[i - 1] + h, ys[i - 1] + k3)
-        ys.append(ys[i - 1] + 1 / 6 * (k1 + 2 * k2 + 2 * k3 + k4))
+    for i in range(len(xs)):
+        k1 = h * f(xs[i], ys[i])
+        k2 = h * f(xs[i] + h / 2, ys[i] + k1 / 2)
+        k3 = h * f(xs[i] + h / 2, ys[i] + k2 / 2)
+        k4 = h * f(xs[i] + h, ys[i] + k3)
+        ys.append(ys[i] + 1 / 6 * (k1 + 2 * k2 + 2 * k3 + k4))
     return ys
 
 
-def milne_method(f, xs, y0, eps=1e-7):
-    ys = fourth_order_runge_kutta_method(f, xs[:4], y0, eps)
+def milne_method(f, xs, y0, eps):
+    n = len(xs)
     h = xs[1] - xs[0]
-    for i in range(4, len(xs)):
-        pre_y = ys[i - 4] + 4 * h / 3 * \
-                (2 * f(xs[i - 3], ys[i - 3]) -
-                 f(xs[i - 2], ys[i - 2]) +
-                 2 * f(xs[i - 1], ys[i - 1]))
-        cor_y = ys[i - 2] + h / 3 * \
-                (f(xs[i - 2], ys[i - 2]) +
-                 4 * f(xs[i - 1], ys[i - 1]) +
-                 f(xs[i], pre_y))
-        while abs(pre_y - cor_y) > eps:
-            pre_y = cor_y
-            cor_y = ys[i - 2] + h / 3 * \
-                    (f(xs[i - 2], ys[i - 2]) +
-                     4 * f(xs[i - 1], ys[i - 1]) +
-                     f(xs[i], pre_y))
-        ys.append(cor_y)
-    return ys
+    y = [y0]
+    for i in range(1, 4):
+        k1 = h * f(xs[i - 1], y[i - 1])
+        k2 = h * f(xs[i - 1] + h / 2, y[i - 1] + k1 / 2)
+        k3 = h * f(xs[i - 1] + h / 2, y[i - 1] + k2 / 2)
+        k4 = h * f(xs[i - 1] + h, y[i - 1] + k3)
+        y.append(y[i - 1] + (k1 + 2 * k2 + 2 * k3 + k4) / 6)
+
+    for i in range(4, n):
+        # Предиктор
+        yp = y[i - 4] + 4 * h * (2 * f(xs[i - 3], y[i - 3]) - f(xs[i - 2], y[i - 2]) + 2 * f(xs[i - 1], y[i - 1])) / 3
+
+        # Корректор
+        y_next = yp
+        while True:
+            yc = y[i - 2] + h * (f(xs[i - 2], y[i - 2]) + 4 * f(xs[i - 1], y[i - 1]) + f(xs[i], y_next)) / 3
+            if abs(yc - y_next) < eps:
+                y_next = yc
+                break
+            y_next = yc
+
+        y.append(y_next)
+
+    return y
